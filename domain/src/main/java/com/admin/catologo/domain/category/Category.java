348fd180a2517fb82cd51acaca9ee708e48ddb97
Daniel Fernandes <daniel.fernandes-contato@outlook.com>
2025-10-19 02:27:32 -0300

    feat: Criando a entidade Jpa de categoria

--- domain/src/main/java/com/admin/catologo/domain/category/Category.java
+++ domain/src/main/java/com/admin/catologo/domain/category/Category.java
@@ -44,6 +44,38 @@ public class Category extends AggregateRoot<CategoryID> {
         return new Category(id, aName, aDescription, active, now, now, deletedAt);
     }
 
+    public static Category with(
+            final CategoryID anId,
+            final String name,
+            final String description,
+            final boolean active,
+            final Instant createdAt,
+            final Instant updatedAt,
+            final Instant deletedAt
+    ) {
+        return new Category(
+                anId,
+                name,
+                description,
+                active,
+                createdAt,
+                updatedAt,
+                deletedAt
+        );
+    }
+
+    public static Category with(final Category aCategory) {
+        return with(
+                aCategory.getId(),
+                aCategory.name,
+                aCategory.description,
+                aCategory.isActive(),
+                aCategory.createdAt,
+                aCategory.updatedAt,
+                aCategory.deletedAt
+        );
+    }
+
     public static Category clone(final Category aCategory) {
         return new Category(
                 aCategory.getId(),
--- infrastructure/src/main/java/com/admin/catologo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/admin/catologo/infrastructure/Main.java
@@ -1,13 +1,16 @@
 package com.admin.catologo.infrastructure;
 
 import com.admin.catologo.infrastructure.config.WebServerConfig;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Main {
      public static void main(String[] args) {
         System.out.println("Infrastructure module");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
     }
 }
